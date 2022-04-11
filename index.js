62f5fc34747a189b8e791b00ccebbb527cd95daa
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-11 11:46:42 -0700

    added dept, roles, and employee to seed

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 // includes MySQL info, but keeps it hidden in the gitignore //
-require('dotenv').config()
+const mySqlP = require('dotenv').config();
+
+const inquirer = require("inquirer");
+const table = require("console.table");
+
+const prompt = require("./config/prompts");
+require("console.table");
+
+// initialize app //
+
+// app start //
+console.log('employee tracker created!');
+
+// employee tracker prompt //
+initialPrompt();
+
+function initialPrompt() {
+    inquirer.prompt(initialPrompt).then(function ({ job }) {
+        switch (job) {
+            // view //
+            case: "View Employees":
+            viewEmployee();
+            break;
+
+            case: "View Employees by Department":
+            viewEmployeeDept();
+            break;
+
+            case: "View Departments":
+            viewDept();
+            break;
+
+            case: "View Department Budget":
+            viewDeptBudget();
+            break;
+            
+            // add //
+
+            case: "Add Employee":
+            addEmployee();
+            break;
+
+            case: "Add Job":
+            addJob();
+            break;
+
+            case: "Add Department"
+            addDepartment();
+            break;
+
+            // update //
+
+            case: "Update Employee"
+            updateEmploye();
+            break;
+        }
+    })
+}
--- sql/seed.sql
+++ sql/seed.sql
@@ -9,3 +9,26 @@ INSERT INTO department (name)
 VALUES ("Finance");
 INSERT INTO department (name)
 VALUES ("Legal");
+
+/* Roles */
+INSERT INTO roles (role)
+VALUES ("id");
+INSERT INTO roles (role)
+VALUES ("title");
+INSERT INTO roles (role)
+VALUES ("salary");
+INSERT INTO roles (role)
+VALUES ("department-id");
+
+
+/* employee */
+INSERT INTO employee (employee)
+VALUES ("id");
+INSERT INTO employee (employees)
+VALUES ("first_name");
+INSERT INTO employee (employees)
+VALUES ("last_name");
+INSERT INTO employee (employees)
+VALUES ("role_id");
+INSERT INTO employee (employee)
+VALUES ("manager_id");
