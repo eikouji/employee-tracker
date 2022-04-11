87778f1a33b9857eabc7e0599076f0d7a8885711
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-11 11:56:43 -0700

    updated schema to look more like hw example

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,30 @@ DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 USE employeeDB;
 
+/* department */
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT INT PRIMARY KEY
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+/* role */
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT
+    title VARCHAR(30) NOT NULL,
+    salary decimal NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+)
+
+
+/* employee */
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INTEGER NULL,
+    manager_id INTEGER,
+    PRIMARY KEY (id)
+)
